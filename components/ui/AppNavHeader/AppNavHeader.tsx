18d97d4558583bd585a48c956f92ce3785b60210
Pradeep Sekar <pradeep87sekar@gmail.com>
2024-02-27 20:51:31 +1100

    Setup and configure push protocol demo
    - Added `<AppThemeToggle />`, Twitter and Calendly button links to the `<AppNavHeader />` component.

--- components/ui/AppNavHeader/AppNavHeader.tsx
+++ components/ui/AppNavHeader/AppNavHeader.tsx
@@ -1,11 +1,37 @@
 import { Container } from "@/components/layout/Container";
 import { AppThemeToggle } from "@/components/providers/AppThemeToggle";
+import { Button } from "@/components/ui/Button";
+import { Calendar } from "lucide-react";
+import Link from "next/link";
+import { TwitterIcon } from "../SVGIcons";
+
+function CalendlyLinkButton() {
+  return (
+    <Button variant="ghost" size="icon" asChild>
+      <Link href="https://calendly.com/grantstream/intro-call" target="_blank">
+        <Calendar className="h-4 w-4" />
+      </Link>
+    </Button>
+  );
+}
+
+function TwitterLinkButton() {
+  return (
+    <Button variant="ghost" size="icon" asChild>
+      <Link href="https://twitter.com/TeamGrantStream" target="_blank">
+        <TwitterIcon className="h-3 w-3" />
+      </Link>
+    </Button>
+  );
+}
 
 export function AppNavHeader() {
   return (
     <header className="fixed top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <Container size="full" className="flex justify-between h-[65px]">
-        <nav className="flex flex-1 items-center space-x-2 justify-end">
+        <nav className="flex flex-1 items-center space-x-1 justify-end">
+          <CalendlyLinkButton />
+          <TwitterLinkButton />
           <AppThemeToggle />
         </nav>
       </Container>
--- components/ui/TeamMembers/data.ts
+++ components/ui/TeamMembers/data.ts
@@ -17,18 +17,18 @@ export const teamMembers: Readonly<TeamMember[]> = [
     address: "0xe563E61293274d15683Ca31363f17C7A2E3B1F6e",
     available: {
       startTime: { hours: 9, minutes: 0 },
-      endTime: { hours: 17, minutes: 0 },
+      endTime: { hours: 14, minutes: 0 },
       tz: "America/Los_Angeles",
     },
   },
   {
     id: 3,
-    name: "Tome Couceiro",
-    address: "0xED3b5B9dFC5238Df12CcE090Db314fe1Ca4a189F",
+    name: "James Boyle",
+    address: "0x4db4Ab89a29c31a9081E0ecA72f3f89a8E44F8d5",
     available: {
-      startTime: { hours: 9, minutes: 0 },
-      endTime: { hours: 17, minutes: 0 },
-      tz: "Europe/Lisbon",
+      startTime: { hours: 14, minutes: 0 },
+      endTime: { hours: 19, minutes: 0 },
+      tz: "America/Los_Angeles",
     },
   },
   {
@@ -37,7 +37,17 @@ export const teamMembers: Readonly<TeamMember[]> = [
     address: "0xE0Aa29E5708ef4077bCde133b27E8046Bb99ca80",
     available: {
       startTime: { hours: 9, minutes: 0 },
-      endTime: { hours: 17, minutes: 0 },
+      endTime: { hours: 14, minutes: 0 },
+      tz: "Europe/Lisbon",
+    },
+  },
+  {
+    id: 5,
+    name: "Tomé Couceiro",
+    address: "0xED3b5B9dFC5238Df12CcE090Db314fe1Ca4a189F",
+    available: {
+      startTime: { hours: 14, minutes: 0 },
+      endTime: { hours: 19, minutes: 0 },
       tz: "Europe/Lisbon",
     },
   },
